e647735a6b2d6acf7297c225943fb6dfbab683a0
rosonsec <36442257+rosonsec@users.noreply.github.com>
2020-05-27 13:51:33 +0200

    Update kali_postinstall.sh

--- kali_postinstall.sh
+++ kali_postinstall.sh
@@ -8,37 +8,41 @@ fi
 
 apt-get update 
 
+# if Virtual Box
 # VirtualBox Guest Additions
 #apt-get install -y virtualbox-guest-x11
 
-# Open Vmware Tools
-apt-get install -y open-vm-tools
+# if Vmware
+## Open Vmware Tools
+#apt-get install -y open-vm-tools
 
-# Settings -> Privacy -> Screen Lock -> Off
-gsettings set org.gnome.desktop.session idle-delay 0
+# if Gnome
+## Settings -> Privacy -> Screen Lock -> Off
+#gsettings set org.gnome.desktop.session idle-delay 0
 
-# Settings -> Power -> Automatic Suspend -> Off
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+## Settings -> Power -> Automatic Suspend -> Off
+#gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+#gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
 
 # Metasploit autostart
-service postgresql start
-update-rc.d postgresql enable
+sudo service postgresql start
+sudo update-rc.d postgresql enable
 
 # Metasploit DB init
-msfdb init
+sudo msfdb init
 #Todo
 #msfconsole -x db_rebuild_cache;exit
 
-# Vim settings
-echo "set tabstop=4" >> /etc/vim/vimrc
-echo "set shiftwidth=4" >> /etc/vim/vimrc
-echo "set softtabstop=4" >> /etc/vim/vimrc
-echo "set expandtab" >> /etc/vim/vimrc
+# Vim user settings
+echo "set tabstop=4" >> ~/.vimrc
+echo "set shiftwidth=4" >> ~/.vimrc
+echo "set softtabstop=4" >> ~/.vimrc
+echo "set expandtab" >> ~/.vimrc
 
 # Terminator
-apt-get install -y terminator
+sudo apt-get install -y terminator
+[ -d ~/.config/terminator ] || mkdir -p  ~/.config/terminator
 wget --no-check-certificate https://raw.githubusercontent.com/rosonsec/Kali/master/conf/terminator.conf -O ~/.config/terminator/config
 
-# System restart
-reboot
+## System restart
+#reboot
